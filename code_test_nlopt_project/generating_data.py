9ab82ca5117f48c56d4cf1c56f27e274b5ea9d9a
David Hunter <dshunter@mit.edu>
2019-05-02 12:58:32 -0400

    files for tests

---
+++ code_test_nlopt_project/generating_data.py
@@ -0,0 +1,31 @@
+import csv
+import numpy as np 
+from sklearn.datasets import load_boston
+from sklearn.datasets import load_breast_cancer
+
+boston = load_boston()
+X = boston.data 
+Y = boston.target 
+data = []
+for num in range(X.shape[0]):
+	entry = X[num,:].tolist()
+	entry.append(Y[num])
+	data.append(entry)
+with open("data_as_csv/boston.csv", 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	for row in data:
+		writer.writerow(row)
+
+
+breast_cancer = load_breast_cancer()
+X = breast_cancer.data 
+Y = breast_cancer.target 
+data = []
+for num in range(X.shape[0]):
+	entry = X[num,:].tolist()
+	entry.append(Y[num])
+	data.append(entry)
+with open("data_as_csv/breast_cancer.csv", 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	for row in data:
+		writer.writerow(row)
